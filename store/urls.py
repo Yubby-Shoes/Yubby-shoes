07446bbba7b8b00402687bdd6018934aa2156e8e
rajesh <rajeshpandey2053@gmail.com>
2020-10-19 15:38:06 +0545

    [Added] configure templates

---
+++ store/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import store, buy
+
+urlpatterns = [
+    path('', store, name='store'),
+    path('buy/', buy, name='cart'),
+
+]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def store(request):
+    context = {}
+    return render(request, 'store/store.html', context)
+
+
+def buy(request):
+    context = {}
+    return render(request, 'store/buy.html', context)
