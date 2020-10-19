776eb5ef647de3a0c8afe926644da9fd889996cd
rajesh <rajeshpandey2053@gmail.com>
2020-10-19 16:06:22 +0545

    feat: configure accounts app

--- store/models.py
+++ store/models.py
@@ -1,15 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-
-class Customer(models.Model):
-    user = models.OneToOneField(
-        User, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=100, null=True)
-    phone_number = models.CharField(max_length=10, null=True)
-
-    def __str__(self):
-        return self.name
 
 
 class Product(models.Model):
