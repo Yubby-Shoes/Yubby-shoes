5d1f73f38db88fdf3001bbd934c76289417b72a5
AnjalBam <anjalbam81@gmail.com>
2020-10-20 02:08:53 +0545

    [feat] add various page views

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html', context={})
+
+
+def shop(request):
+    return render(request, 'shop.html', context={})
+
+
+def about(request):
+    return render(request, 'about.html', context={})
+
+
+def blog(request):
+    return render(request, 'blog.html', context={})
+
+
+def contact(request):
+    return render(request, 'contact.html', context={})
