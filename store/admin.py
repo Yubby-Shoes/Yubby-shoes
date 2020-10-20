c10240d430aac6c934647a95d7dfd6daac7ef9d9
AnjalBam <anjalbam81@gmail.com>
2020-10-21 00:38:40 +0545

    [add] register models to admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, Company, Category
 
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Company)
+admin.site.register(Category)
