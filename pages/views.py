53b08f985912b6576e73ca7cf48812237bf823b9
AnjalBam <anjalbam81@gmail.com>
2020-10-21 01:58:39 +0545

    [feat] modify index and shop view to pass dynamic data

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponseNotAllowed
+from django.core.paginator import Paginator
+from store.models import *
 
 
 def index(request):
-    return render(request, 'index.html', context={})
+    if request.method == 'GET':
+        # to return just 9 latest products
+        products = Product.objects.all().order_by('-date_added')[:9]
+        # print('products=', len(products))
+        context = {
+            'products': products
+        }
+        return render(request, 'index.html', context=context)
+    return HttpResponseNotAllowed('Methods Other than get not allowed!')
 
 
 def shop(request):
-    return render(request, 'shop.html', context={})
+    if request.method == 'GET':
+        products = Product.objects.all()
+        p = Paginator(products, 9)
+        page_no = request.GET.get('page', 1)
+        paginated_products = p.get_page(page_no)
+        return render(request, 'shop.html', context={
+            'products': paginated_products
+        })
+    return HttpResponseNotAllowed('Methods Other than get not allowed!')
 
 
 def about(request):
