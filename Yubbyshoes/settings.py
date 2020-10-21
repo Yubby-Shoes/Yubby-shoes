d4e42e908b56d59016b5ff8ccda92aadae36dd85
rajesh <rajeshpandey2053@gmail.com>
2020-10-21 14:18:35 +0545

    feat: desiging store

--- Yubbyshoes/settings.py
+++ Yubbyshoes/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'store',
     'accounts',
     'pages',
@@ -126,3 +127,5 @@ STATIC_ROOT = '/assets/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
