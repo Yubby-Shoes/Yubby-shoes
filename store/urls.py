bb45a394d2bd508fb0749d19c0aadf7388dc14e8
rajesh <rajeshpandey2053@gmail.com>
2020-10-29 20:32:02 +0545

    fix: removed store list view with filter

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import Create, List, Detail, Update, Delete, filter_products_bycategory
+from .views import Create, Detail, Update, Delete, filter_products_bycategory
 
 app_name = 'store'
 
 urlpatterns = [
     # path('', store, name='store'),
     path('create/', Create.as_view(), name="create"),
-    path('list/', List.as_view(), name="list"),
+    # path('list/', List.as_view(), name="list"),
     path('filter/<str:category>/', filter_products_bycategory, name="filter"),
     path('detail/<int:pk>/', Detail.as_view(), name="detail"),
     path('update/<int:pk>/', Update.as_view(), name='update'),
