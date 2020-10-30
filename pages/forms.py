f76c8cff2f7b70d32f006c6a108989b1c876c6fa
AnjalBam <anjalbam81@gmail.com>
2020-10-30 18:12:10 +0545

    [add] add user buying information form

--- pages/forms.py
+++ pages/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from store.models import OrderItem, Product
+from django.shortcuts import get_object_or_404
 
 
 class UserDetailForm(forms.Form):
@@ -6,6 +8,7 @@ class UserDetailForm(forms.Form):
     phone_number = forms.CharField(max_length=15)
     email = forms.EmailField(required=False)
     address = forms.CharField(max_length=255, widget=forms.Textarea)
+    quantity = forms.IntegerField(required=True, widget=forms.NumberInput)
 
     full_name.widget.attrs.update({
         'placeholder': 'Full Name'
@@ -20,3 +23,25 @@ class UserDetailForm(forms.Form):
         'placeholder': 'Address',
         'rows': 4
     })
+    quantity.widget.attrs.update({
+        'placeholder': 'Quantity',
+        'value': 1
+    })
+
+    def clean_quantity(self):
+        qty = self.cleaned_data['quantity']
+        if not isinstance(qty, int):
+            raise forms.ValidationError('Quantity must be an integer')
+        return qty
+
+    def save(self, product_id):
+        product = get_object_or_404(Product, pk=product_id)
+        order_item = OrderItem()
+        order_item.product = product
+        order_item.order_name = self.cleaned_data['full_name']
+        order_item.order_address = self.cleaned_data['address']
+        order_item.order_email = self.cleaned_data['email']
+        order_item.order_quantity = self.cleaned_data['quantity']
+        order_item.order_phone = self.cleaned_data['phone_number']
+        order_item.save()
+        return order_item
