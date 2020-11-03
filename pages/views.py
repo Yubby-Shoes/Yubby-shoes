9db6d6799ad02ab66518dd17d1512fcecc65068c
rajesh <rajeshpandey2053@gmail.com>
2020-11-03 13:47:24 +0545

    feat: woooomen shoes

--- pages/views.py
+++ pages/views.py
@@ -46,6 +46,27 @@ def shop(request):
     return HttpResponseNotAllowed('Methods Other than get not allowed!')
 
 
+def shop_women(request):
+    if request.method == 'GET':
+        products = Product.objects.filter(
+            category__category_name__startswith="women").order_by('-date_added')
+        cg = request.GET.get('category')
+
+        if cg:
+            category = get_object_or_404(Category, pk=cg)
+            products = category.product_set.all().order_by('-date_added')
+            # print(products)
+
+        p = Paginator(products, 9)
+        page_no = request.GET.get('page', 1)
+        paginated_products = p.get_page(page_no)
+        return render(request, 'pages/shop.html', context={
+            'products': paginated_products,
+            'categories': Category.objects.filter(category_name__startswith="women")
+        })
+    return HttpResponseNotAllowed('Methods Other than get not allowed!')
+
+
 def item_detail(request, pk):
     if request.method == 'GET':
         item = get_object_or_404(Product, pk=pk)
