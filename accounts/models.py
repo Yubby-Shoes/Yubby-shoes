e09ec9d982f956a453020cdfe4cdefeaf1edefad
AnjalBam <anjalbam81@gmail.com>
2020-11-06 21:50:04 +0545

    [add] model changes

--- accounts/models.py
+++ accounts/models.py
@@ -6,8 +6,10 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=100, null=True)
-    phone_number = models.CharField(max_length=10, null=True)
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=10, unique=True)
+    address = models.CharField(max_length=255)
+    default_size = models.CharField(max_length=10, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.name + '-' + self.phone_number
